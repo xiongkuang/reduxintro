e6b913d6c35d22f7059a2d715371900443954381
xiongkuang <xiongkuang@gmail.com>
2016-10-25 11:05:32 -0400

    frst

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'JS', pages: 1000},
+        { title: 'HP', pages: 2000},
+        { title: 'DT', pages: 3000},
+        { title: 'ER', pages: 4000}
+    ];
+}
